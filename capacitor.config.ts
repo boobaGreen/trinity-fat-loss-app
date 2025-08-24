d6805a10f04ea733dd53a5c2648f567f6b2f8660
Claudio Dall'Ara <claudio.dallara@agilelab.it>
2025-08-24 16:13:35 +0200

    html update and logo svg

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.trinity.fatloss',
+  appName: 'Trinity Fat Loss',
+  webDir: 'build'
+};
+
+export default config;
