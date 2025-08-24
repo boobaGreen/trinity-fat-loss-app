850f30fc285f35f46c3ddf85ed92cf415e682fe9
Claudio Dall'Ara <claudio.dallara@agilelab.it>
2025-08-24 19:46:45 +0200

    androi studio 1 test ok

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.trinity.fatloss',
-  appName: 'Trinity Fat Loss',
-  webDir: 'build'
+  appId: "com.trinity.fatloss",
+  appName: "Trinity Fat Loss",
+  webDir: "dist",
 };
 
 export default config;
